a71f5676b803fd68058a6b0ce9cc6b46a34699be
chenlt <chenlt@wayyue.com>
2020-12-24 00:01:57 +0800

    2020-12-23 387. 字符串中的第一个唯一字符

---
+++ dayOne/src/main/java/d20201223/T387FirstUniqChar.java
@@ -0,0 +1,30 @@
+package d20201223;
+
+/**
+ * @author Chen Liting
+ * @version 1.0.0
+ * @className T387FirstUniqChar
+ * @Description 字符串中的第一个唯一字符
+ * @date 2020-12-24
+ */
+public class T387FirstUniqChar {
+    /**
+     * https://leetcode-cn.com/problems/first-unique-character-in-a-string/
+     *
+     * @param s 字符串
+     * @return 下标
+     */
+    public int firstUniqChar(String s) {
+        int[] times = new int[26];
+        char[] cs = s.toCharArray();
+        for (char c : cs) {
+            times[c - 'a']++;
+        }
+        for (int i = 0; i < cs.length; ++i) {
+            if (times[cs[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
