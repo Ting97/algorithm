3e70762e16305341a56e040f2b0edb52b41af9c0
chenliting <chenlt@wayyue.com>
2021-05-07 20:39:24 +0800

    2021.05.07 MT0108 零矩阵 T498 对角线遍历

---
+++ dayOne/src/main/java/cn/ting97/year2021/aug/d7/MT0108ZeroMatrixLCCI.java
@@ -0,0 +1,35 @@
+package cn.ting97.year2021.aug.d7;
+
+/**
+ * @author Chen Liting
+ * @version 1.0.0
+ * @className MT0108ZeroMatrixLCCI
+ * @Description 零矩阵
+ * @date 2021-05-07
+ */
+public class MT0108ZeroMatrixLCCI {
+    /**
+     * 编写一种算法，若M × N矩阵中某个元素为0，则将其所在的行与列清零。
+     *
+     * @param matrix 矩阵
+     */
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] mz = new boolean[m];
+        boolean[] nz = new boolean[n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == 0) {
+                    mz[i] = true;
+                    nz[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix[i][j] = mz[i] || nz[j] ? 0 : matrix[i][j];
+            }
+        }
+    }
+}
---
+++ dayOne/src/main/java/cn/ting97/year2021/aug/d7/T498DiagonalTraverse.java
@@ -0,0 +1,40 @@
+package cn.ting97.year2021.aug.d7;
+
+/**
+ * @author Chen Liting
+ * @version 1.0.0
+ * @className T498DiagonalTraverse
+ * @Description 对角线遍历
+ * @date 2021-05-07
+ */
+public class T498DiagonalTraverse {
+    /**
+     * 给定一个含有 M x N 个元素的矩阵（M 行，N 列），请以对角线遍历的顺序返回这个矩阵中的所有元素，对角线遍历如下图所示。
+     *
+     * @param mat 矩阵
+     * @return 返回对角线遍历
+     */
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] ans = new int[m * n];
+        int index = 0;
+        int x, y;
+        for (int i = 0; i < m + n - 1; ++i) {
+            if (i % 2 == 0) {
+                y = Math.min(i, m - 1);
+                x = i - Math.min(i, m - 1);
+                while (y >= 0 && x < n) {
+                    ans[index++] = mat[y--][x++];
+                }
+            } else {
+                x = Math.min(i, n - 1);
+                y = i - Math.min(i, n - 1);
+                while (x >= 0 && y < m) {
+                    ans[index++] = mat[y++][x--];
+                }
+            }
+        }
+        return ans;
+    }
+}
