ab8088229afdbbbbf98aa5ff3743a2c59e863de8
ting97 <litingchen97@163.com>
2021-05-11 00:10:37 +0800

    2021.05.10 T209 长度最小的子数组

--- dayOne/src/main/java/cn/ting97/year2021/may/d8/T209MinimumSizeSubarraySum.java
+++ dayOne/src/main/java/cn/ting97/year2021/may/d8/T209MinimumSizeSubarraySum.java
@@ -9,15 +9,24 @@ package cn.ting97.year2021.may.d8;
  */
 public class T209MinimumSizeSubarraySum {
     /**
-     * 给定一个含有 n 个正整数的数组和一个正整数 target 。
+     * 给定一个含有 n 个正整数的数组和一个正整数 target 。
      * <p>
-     * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+     * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
      *
      * @param target 目标长度
      * @param nums   数组
      * @return 长度
      */
     public int minSubArrayLen(int target, int[] nums) {
-        return 0;
+        int ans = Integer.MAX_VALUE;
+        int sum = 0;
+        for (int i = 0, j = 0; j < nums.length; ++j) {
+            sum += nums[j];
+            while (sum >= target) {
+                ans = Math.min(ans, j - i + 1);
+                sum -= nums[i++];
+            }
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
     }
 }
